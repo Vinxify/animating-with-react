96a05bd500886a284f14907de4293273a768582b
Vinxify <babatundesegun123@gmail.comm>
2024-02-28 16:01:12 +0100

    second commit

--- src/components/ChallengeItem.jsx
+++ src/components/ChallengeItem.jsx
@@ -1,6 +1,6 @@
-import { useContext } from 'react';
+import { useContext } from "react";
 
-import { ChallengesContext } from '../store/challenges-context.jsx';
+import { ChallengesContext } from "../store/challenges-context.jsx";
 
 export default function ChallengeItem({
   challenge,
@@ -10,49 +10,51 @@ export default function ChallengeItem({
   const { updateChallengeStatus } = useContext(ChallengesContext);
 
   const formattedDate = new Date(challenge.deadline).toLocaleDateString(
-    'en-US',
+    "en-US",
     {
-      day: '2-digit',
-      month: 'short',
-      year: 'numeric',
+      day: "2-digit",
+      month: "short",
+      year: "numeric",
     }
   );
 
   function handleCancel() {
-    updateChallengeStatus(challenge.id, 'failed');
+    updateChallengeStatus(challenge.id, "failed");
   }
 
   function handleComplete() {
-    updateChallengeStatus(challenge.id, 'completed');
+    updateChallengeStatus(challenge.id, "completed");
   }
 
   return (
     <li>
-      <article className="challenge-item">
+      <article className='challenge-item'>
         <header>
           <img {...challenge.image} />
-          <div className="challenge-item-meta">
+          <div className='challenge-item-meta'>
             <h2>{challenge.title}</h2>
             <p>Complete until {formattedDate}</p>
-            <p className="challenge-item-actions">
-              <button onClick={handleCancel} className="btn-negative">
+            <p className='challenge-item-actions'>
+              <button onClick={handleCancel} className='btn-negative'>
                 Mark as failed
               </button>
               <button onClick={handleComplete}>Mark as completed</button>
             </p>
           </div>
         </header>
-        <div className="challenge-item-details">
+        <div
+          className={`challenge-item-details ${isExpanded ? "expanded" : ""}`}
+        >
           <p>
             <button onClick={onViewDetails}>
-              View Details{' '}
-              <span className="challenge-item-details-icon">&#9650;</span>
+              View Details{" "}
+              <span className='challenge-item-details-icon'>&#9650;</span>
             </button>
           </p>
 
           {isExpanded && (
             <div>
-              <p className="challenge-item-description">
+              <p className='challenge-item-description'>
                 {challenge.description}
               </p>
             </div>
